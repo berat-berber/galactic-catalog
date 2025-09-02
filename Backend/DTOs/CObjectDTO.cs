35a3752048eb437dc8c520f4dfebc5f873969ced
berat-berber <berat0berber@gmail.com>
2025-09-02 16:15:19 +0300

    create dto for user and celestial object

---
+++ Backend/DTOs/CObjectDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend;
+
+public record class CObjectDTO
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DiscoveredAt { get; set; }
+    public string ImageUrl { get; set; }
+}
---
+++ Backend/DTOs/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend;
+
+public record class UserDTO
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string RawPassword { get; set; }
+}
