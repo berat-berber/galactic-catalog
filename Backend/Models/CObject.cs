54c032026ac2a16ca0b21c1d6fc0297793999dd5
berat-berber <berat0berber@gmail.com>
2025-09-02 16:07:56 +0300

    create user and celestial object models

---
+++ Backend/Models/CObject.cs
@@ -0,0 +1,23 @@
+﻿namespace Backend;
+
+[Table("c_objects")]
+public class CObject
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("type")]
+    public string Type { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
+
+    [Column("discovered_at")]
+    public DateTime DiscoveredAt { get; set; }
+
+    [Column("image_url")]
+    public string ImageUrl { get; set; }
+}
---
+++ Backend/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Backend;
+
+[Table("users")]
+public class User
+{
+    [Column("id")]
+    public Guid Id { get; set; }
+
+    [Column("username")]
+    public string Username { get; set; }
+
+    [Column("email")]
+    public string Email { get; set; }
+
+    [Column("hashed_password")]
+    public string HashedPassword { get; set; }
+
+    [Column("role")]
+    public string Role { get; set; }
+}
