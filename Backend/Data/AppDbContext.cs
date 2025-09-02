35e625e4022b8c5d0d91ae8fc966a70533c9b852
berat-berber <berat0berber@gmail.com>
2025-09-02 16:21:45 +0300

    download packages and configure postgresql connection

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend;
+
+public class AppDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<CObject> CObjects { get; set; }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("database")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
