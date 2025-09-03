c460bd30757ab5c1cec08b4d74373a0c8d8ebe4c
berat-berber <berat0berber@gmail.com>
2025-09-03 15:09:42 +0300

    remove upload image endpoint

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0675ce55e4465f570bbf471bc6bceb8c26e11158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fe09e9de056c9392292f91272fa08ba07941233")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
